bc16881bbdb76a7738208d7ad894ff2aa582f20a
Nisar Ahmed <53566249+nisarr@users.noreply.github.com>
2021-10-10 19:56:42 +0500

    Update lara-response.php

--- src/helpers/lara-response.php
+++ src/helpers/lara-response.php
@@ -11,3 +11,47 @@ if(!function_exists('api_response')){
 	}
 
 }
+
+if(!function_exists('apiResponse')){
+
+	function apiResponse($data = '',$status = 200,$msg = '')
+	{
+	    return LaraResponse::json($data, $status, $msg);
+	}
+
+}
+
+if(!function_exists('successResponse')){
+
+	function successResponse($msg = '',$data = '')
+	{
+	    return LaraResponse::json($data, 200, $msg);
+	}
+
+}
+
+if(!function_exists('errorResponse')){
+
+	function errorResponse($data = '',$msg = '',$status = 400)
+	{
+	    return LaraResponse::json($data, $status, $msg);
+	}
+
+}
+
+if(!function_exists('error404Response')){
+
+	function error404Response($msg = 'Not Found',$data = '')
+	{
+	    return LaraResponse::json($data,404, $msg);
+	}
+}
+
+if(!function_exists('error400Response')){
+
+	function error400Response($data = '',$msg = 'Validation Error')
+	{
+	    return LaraResponse::json($data,400, $msg);
+	}
+
+}
